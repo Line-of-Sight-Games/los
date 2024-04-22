145fe28dff6e03f8d30384fd63c5fc8a04cbd2cf
SpyraxianJ <93021078+SpyraxianJ@users.noreply.github.com>
2024-04-22 16:30:39 +1000

    Scriptify InsertObjectsUI
    Remove claymores as POI
    Scriptified

---
+++ Assets/Scripts/UI/InsertObjectsUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InsertObjectsUI : MonoBehaviour
+{
+    public TMP_Dropdown objectTypeDropdown;
+    public TMP_InputField xPos;
+    public TMP_InputField yPos;
+    public TMP_InputField zPos;
+    public TMP_Dropdown terrainDropdown;
+    public TMP_Dropdown terminalTypeDropdown;
+
+    public Transform gbItemsPanel;
+
+    public GameObject terminalTypeUI;
+    public GameObject allItemsPanelUI;
+}
