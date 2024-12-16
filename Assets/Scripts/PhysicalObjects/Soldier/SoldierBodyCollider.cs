9f01282aa0fb3f35a7358447c08c54de6bf68f78
SpyraxianJ <93021078+SpyraxianJ@users.noreply.github.com>
2024-12-16 11:25:04 +1100

    Dynamic Sizing of soldiers
    FEATURES:
    - Soldiers change size based on state
    - Active = 2 units
    - LS = 1 unit
    - Uncon/Playdead/Dead = 0.5 units

--- Assets/Scripts/PhysicalObjects/Soldier/SoldierBodyCollider.cs
+++ Assets/Scripts/PhysicalObjects/Soldier/SoldierBodyCollider.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SoldierBodyCollider : BaseBodyCollider
 {
     public Soldier linkedSoldier;
+    public GameObject physicalSoldier;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +13,22 @@ public class SoldierBodyCollider : BaseBodyCollider
     // Update is called once per frame
     void Update()
     {
-        
+        if (linkedSoldier.IsDead() || linkedSoldier.IsUnconscious() || linkedSoldier.IsPlayingDead())
+        {
+            //doesn't work cause blocks soldier physical from moving
+            physicalSoldier.transform.position = new(physicalSoldier.transform.position.x, 0.5f, physicalSoldier.transform.position.z);
+            physicalSoldier.transform.localScale = new(physicalSoldier.transform.localScale.x, 0.5f, physicalSoldier.transform.localScale.z);
+        }
+        else if (linkedSoldier.IsLastStand())
+        {
+            physicalSoldier.transform.position = new(physicalSoldier.transform.position.x, 1, physicalSoldier.transform.position.z);
+            physicalSoldier.transform.localScale = new(physicalSoldier.transform.localScale.x, 1, physicalSoldier.transform.localScale.z);
+        }
+        else
+        {
+            physicalSoldier.transform.position = new(physicalSoldier.transform.position.x, 2, physicalSoldier.transform.position.z);
+            physicalSoldier.transform.localScale = new(physicalSoldier.transform.localScale.x, 2, physicalSoldier.transform.localScale.z);
+        }
     }
 
     public Soldier LinkedSoldier
