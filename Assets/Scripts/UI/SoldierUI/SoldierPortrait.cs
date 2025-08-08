cc91f5a0f818d559ac8f7a02ee929fd2193c07db
SpyraxianJ <93021078+SpyraxianJ@users.noreply.github.com>
2025-08-08 14:35:04 +1000

    Bugfix to crash when loading portraits with headgear
    BUGFIXES:
    - Fixed a bug where game would sometimes crash during load when portraits included headgear

--- Assets/Scripts/UI/SoldierUI/SoldierPortrait.cs
+++ Assets/Scripts/UI/SoldierUI/SoldierPortrait.cs
@@ -23,60 +23,63 @@ public class SoldierPortrait : MonoBehaviour
 
     private void Update()
     {
-        if (linkedSoldier != null)
+        if (ItemManager.Instance.IsDataLoaded && MenuManager.Instance.IsDataLoaded)
         {
-            //load team colours
-            if (linkedSoldier.soldierTeam == 1)
-                teamIndicator.color = Color.red;
-            else
-                teamIndicator.color = Color.blue;
-
-            //load portrait
-            portrait.sprite = linkedSoldier.soldierPortrait;
-
-            //load rank
-            soldierRank.sprite = linkedSoldier.LoadInsignia(linkedSoldier.rank);
-
-            //load hp
-            if (linkedSoldier.IsOnturn() || MenuManager.Instance.OverrideView)
+            if (linkedSoldier != null)
             {
-                armourImage.gameObject.SetActive(false);
-                brokenArmourImage.gameObject.SetActive(false);
-                if (linkedSoldier.IsWearingBodyArmour(true) || linkedSoldier.IsWearingJuggernautArmour(true))
-                    armourImage.gameObject.SetActive(true);
-                else if (linkedSoldier.IsWearingBodyArmour(false) || linkedSoldier.IsWearingJuggernautArmour(false))
-                    brokenArmourImage.gameObject.SetActive(true);
-                hpDisplayImage.gameObject.SetActive(true);
-                hpDisplay.text = $"{linkedSoldier.GetFullHP()}";
+                //load team colours
+                if (linkedSoldier.soldierTeam == 1)
+                    teamIndicator.color = Color.red;
+                else
+                    teamIndicator.color = Color.blue;
+
+                //load portrait
+                portrait.sprite = linkedSoldier.soldierPortrait;
+
+                //load rank
+                soldierRank.sprite = linkedSoldier.LoadInsignia(linkedSoldier.rank);
+
+                //load hp
+                if (linkedSoldier.IsOnturn() || MenuManager.Instance.OverrideView)
+                {
+                    armourImage.gameObject.SetActive(false);
+                    brokenArmourImage.gameObject.SetActive(false);
+                    if (linkedSoldier.IsWearingBodyArmour(true) || linkedSoldier.IsWearingJuggernautArmour(true))
+                        armourImage.gameObject.SetActive(true);
+                    else if (linkedSoldier.IsWearingBodyArmour(false) || linkedSoldier.IsWearingJuggernautArmour(false))
+                        brokenArmourImage.gameObject.SetActive(true);
+                    hpDisplayImage.gameObject.SetActive(true);
+                    hpDisplay.text = $"{linkedSoldier.GetFullHP()}";
+                }
+                else //hide all hp details 
+                {
+                    armourImage.gameObject.SetActive(false);
+                    brokenArmourImage.gameObject.SetActive(false);
+                    hpDisplayImage.gameObject.SetActive(false);
+                    hpDisplay.text = string.Empty;
+                }
+
+                //load body position
+                if (linkedSoldier.IsUnconscious() || linkedSoldier.IsPlayingDead())
+                    soldierPosition.sprite = linkedSoldier.LoadPosition("Unconscious");
+                else if (linkedSoldier.IsLastStand())
+                    soldierPosition.sprite = linkedSoldier.LoadPosition("Last Stand");
+                else
+                    soldierPosition.sprite = linkedSoldier.LoadPosition("Active");
+
+                //load equipment on head
+                if (linkedSoldier.InventorySlots["Head"] != "")
+                    equipmentOnHead.sprite = linkedSoldier.LoadHeadEquipment(ItemManager.Instance.FindItemById(linkedSoldier.InventorySlots["Head"]).itemName);
+                else
+                    equipmentOnHead.sprite = linkedSoldier.LoadHeadEquipment("Nothing");
+
+                //load name
+                soldierName.text = linkedSoldier.soldierName;
+
+                //load location
+                if (soldierLocation != null)
+                    soldierLocation.text = $"X:{linkedSoldier.X} Y:{linkedSoldier.Y} Z:{linkedSoldier.Z}";
             }
-            else //hide all hp details 
-            {
-                armourImage.gameObject.SetActive(false);
-                brokenArmourImage.gameObject.SetActive(false);
-                hpDisplayImage.gameObject.SetActive(false);
-                hpDisplay.text = string.Empty;
-            }
-
-            //load body position
-            if (linkedSoldier.IsUnconscious() || linkedSoldier.IsPlayingDead())
-                soldierPosition.sprite = linkedSoldier.LoadPosition("Unconscious");
-            else if (linkedSoldier.IsLastStand())
-                soldierPosition.sprite = linkedSoldier.LoadPosition("Last Stand");
-            else
-                soldierPosition.sprite = linkedSoldier.LoadPosition("Active");
-
-            //load equipment on head
-            if (linkedSoldier.InventorySlots["Head"] != "")
-                equipmentOnHead.sprite = linkedSoldier.LoadHeadEquipment(ItemManager.Instance.FindItemById(linkedSoldier.InventorySlots["Head"]).itemName);
-            else
-                equipmentOnHead.sprite = linkedSoldier.LoadHeadEquipment("Nothing");
-
-            //load name
-            soldierName.text = linkedSoldier.soldierName;
-
-            //load location
-            if (soldierLocation != null)
-                soldierLocation.text = $"X:{linkedSoldier.X} Y:{linkedSoldier.Y} Z:{linkedSoldier.Z}";
         }
     }
 }
