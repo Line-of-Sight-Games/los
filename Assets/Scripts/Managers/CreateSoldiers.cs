1ed73b92cf49f18a34fc12eadd706083a7b4c9fa
SpyraxianJ <93021078+SpyraxianJ@users.noreply.github.com>
2025-12-24 11:37:38 +1100

    Bugfix brutus returns
    BUGFIXES:
    - Brutus now builds correctly when using random generation

--- Assets/Scripts/Managers/CreateSoldiers.cs
+++ Assets/Scripts/Managers/CreateSoldiers.cs
@@ -328,7 +328,6 @@ public class CreateSoldiers : MonoBehaviour, IDataPersistence
 			ConfirmButtonPressed();
 			UpdateVisuals();
         }
-        ConfirmButtonPressed();
     }
     public IEnumerator RandomZombieTeam()
     {
